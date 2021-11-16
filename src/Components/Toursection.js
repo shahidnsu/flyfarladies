fc9c81f170dca951799f7be6ee53baf19e49fb2f
Shahidnsu <shahid.khan@northsouth.edu>
2021-11-16 21:47:28 +0600

    removed some unused codes

--- src/Components/Toursection.js
+++ src/Components/Toursection.js
@@ -13,7 +13,7 @@ const Toursection = () => {
             <Row className="ms-5 mt-5">
             <Col className="d-flex flex-column align-items-center">
                 <p className="h1 mt-2">Most Popular Tours</p>
-                <p className="h5 text-secondary mt-4 text-center fs-3 lh-2">Check out the Most Popular Tours deals below to book your holiday for the best price! Various promotions run throughout the year, so keep an eye out to bag the hottest of bargains for unforgettable experiences.
+                <p className="h5 text-secondary mt-4 text-center fs-3 lh-1">Check out the Most Popular Tours deals below to book your holiday for the best price! Various promotions run throughout the year, so keep an eye out to bag the hottest of bargains for unforgettable experiences.
                 </p>
                 </Col>
                 </Row>
