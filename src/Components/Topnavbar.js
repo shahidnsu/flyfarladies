4f99b8a00284087ff63d45d3ebe8554e47769d30
Shahidnsu <shahid.khan@northsouth.edu>
2021-11-16 14:04:04 +0600

    completed the topnavbar section and and moved the topnavbar section to the component folder

---
+++ src/Components/Topnavbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Container,Row,Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus,faPhoneSquareAlt } from "@fortawesome/free-solid-svg-icons";
+
+const Topnavbar = () => {
+
+
+    return(
+
+        <Container fluid className="border p-3 bg-light">
+        <Row className="pt-2 d-flex justify-content-center ">
+           <Col className="d-flex justify-content-start ">
+           <FontAwesomeIcon   className = "pe-1" icon={faPhoneSquareAlt} size="2x"/>
+                <h5>0175558211, info@flyfarladies.com</h5> 
+            </Col>
+
+            <Col   className="d-flex justify-content-end">
+
+                <Button  className ="rounded-pill border-white" variant="dark">Login/Register</Button>
+                
+                <FontAwesomeIcon  className="mt-1" icon= {faCartPlus} size="2x" />
+                </Col>   
+                
+                
+                
+        
+
+        </Row>
+            
+    </Container>
+
+
+    )
+}
+export default  Topnavbar
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import Topnavbar from "./Components/Topnavbar";
+
+
+
+const Home = () => {
+    return (
+        
+        <div>
+        <Topnavbar />
+
+       
+
+        </div>
+    )
+}
+export default Home;
