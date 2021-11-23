869cd756a1dc54236cd7b72f08f1c177d2d0592d
Shahidnsu <shahid.khan@northsouth.edu>
2021-11-23 14:23:10 +0600

    redesgin the job section of the page

--- src/Components/Jobsectioncard.js
+++ src/Components/Jobsectioncard.js
@@ -4,37 +4,41 @@ import { Card } from "react-bootstrap";
 import test from '../assets/images/test.jpeg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-regular-svg-icons";
+import '../assets/style/jobsectioncard.css';
 
 const Jobsectioncard =({name,number}) => {
     return (
 
-        <div >
-             <Card  className ="shadow-lg"style={{ width: '20rem' ,height : '28rem'}}>
+        <div>
+             <Card  className ="shadow"style={{ width: '20rem' ,height : '27rem'}}>
        <Card.Img variant="top" src={test} />
-         <Card.Body>
+         <Card.Body className="job-section-details">
          
           
            
-    <div className="d-flex justify-content-between">
-        <div >
+    <div className="d-flex justify-content-between  ">
 
-        <p className="mt-2 h5"> {name}  </p>
-        <p className="mt-2 h5">{number} Jobs</p>
+    <div>
+
+        <p className="job-section-details-text "> {name}  </p>
+        <p className="job-section-details-text ">{number} Jobs</p>
         
 
         </div>
 
         <div >
 
-        <p className="mt-2 h5">  <FontAwesomeIcon icon={faBookmark} size="2x"/></p>
+        <p className="mt-2 h5 job-section-details-text">  <FontAwesomeIcon icon={faBookmark} size="2x"/></p>
         
 
         </div>
 
 
     </div>
+    
   </Card.Body>
 </Card>
+
         </div>
 
     )
