1a69f7b583a8ba605d38968feaa64468df32ab69
Shahidnsu <shahid.khan@northsouth.edu>
2021-11-23 15:43:22 +0600

    added some styles for footer section and need to redesgin the links for mobile

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,9 @@
-
+import React from 'react';
 import { faFacebook, faGoogle, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+
 import { Container,Row,Col,Stack,Button } from 'react-bootstrap';
+import '../assets/style/footer.css'
 
 const Footer =() => {
 
@@ -14,18 +15,27 @@ const Footer =() => {
                     <p className="h2 text-center"> READY FOR NEXT ADVENTURE?</p>
                     <div className="d-flex mt-5">
                     <Stack gap={3} className="col-md-2 col-sm-12 col-xs-12 col-lg-2">
-                    <input   type="name" placeholder="First Name.."></input>
-                    <input type="name" placeholder="Email ID.."></input>
-                    <input type="name" placeholder="Travel Date.."></input>
-                    <input type ="textArea" placeholder="Tell us about your dream  adventure.."></input>
+                    <ul className="list-group list-group-flush">
+                    <input type="text" className="list-group-item" placeholder="First Name.."></input>
+                    <input type="email" className="list-group-item" placeholder="Email ID..."></input>
+                    <input type="date" className="list-group-item" placeholder="Travel Date.."></input>
+                    <input type="textarea" className="list-group-item" placeholder="Tell us about your dream adventure"></input>
+                    
+                          
+    </ul>  
                         
                     </Stack>
 
                     <Stack gap={3} className="col-md-2 col-sm-12 col-xs-12 col-lg-2 mx-3">
-                    <input type="name" placeholder="Last Name.."></input>
-                    <input type="name" placeholder="Contact No."></input>
-                    <input type="name" placeholder="Number of People.."></input>
-                    <Button variant="outline-secondary">Secondary</Button>
+                    <ul className="list-group list-group-flush">
+                         <input type="text" className="list-group-item" placeholder="Last Name.."></input>
+                         <input type="tel" className="list-group-item" placeholder="Contact No.."></input>
+                         <input type="number" className="list-group-item" placeholder="Number of People.."></input>
+                         <button className="footer-submit-button"> Submit</button>
+  
+  
+</ul>
+                
                         
                     </Stack>
                     </div>
@@ -36,12 +46,13 @@ const Footer =() => {
                 <p className="h2 text-center">QUICK LINKS</p>
                 <div className="mt-3 mx-3">
                    <Stack gap={3}>
-                   <a href="#">Home</a>
-                   <a href="#">About Us</a>
-                   <a href="#">Contact Us</a>
-                   <a href="#">Terms and Conditions</a>
-                   <a href="#">Privacy Policy</a>
-                   <a href="#">Refund Policy</a>
+                   <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Home</li>
+                      <li class="list-group-item">About Us</li>
+                    <li class="list-group-item">Contact Us</li>
+                     <li class="list-group-item">Terms and Conditions</li>
+                     <li class="list-group-item">Refund Policy </li>
+</ul>
                    </Stack>
                 </div>
 
