89785cb9a917cea17aa049b6fe9a6b945747fae5
Shahidnsu <shahid.khan@northsouth.edu>
2021-11-25 01:26:39 +0600

    solved the responsive issue of the topnavbar

--- src/Components/Topnavbar.js
+++ src/Components/Topnavbar.js
@@ -11,13 +11,27 @@ const Topnavbar = () => {
     return(
 
         <Container fluid className="border p-3 bg-light">
+
+        <Row>
+            <Col sm="12" xs="12" md="6" lg="6" className="d-flex justify-content-sm-center justify-content-xs-center justify-content-md-start justify-content-lg-start">
+            <FontAwesomeIcon   className = "algin-self-baseline" icon={faPhoneSquareAlt} size="2x"/>
+                <h5 className="align-self-center ms-1">0175558211, info@flyfarladies.com</h5>
+            </Col>
+            <Col className="d-flex justify-content-md-end justify-content-lg-end justify-content-sm-center justify-content-xs-center">
+            <Button  className ="rounded-pill border-white" variant="dark">Login/Register</Button>
+                
+                <FontAwesomeIcon  className="mt-1" icon= {faCartPlus} size="2x" />
+            </Col>
+        </Row>
+
+        {/*
         <Row className="pt-2 d-flex">
-           <Col  lg={6}className="d-flex justify-content-start">
+           <Col  lg={6} md="6" sm= "12" xs ="12" className="d-flex justify-content-start">
            <FontAwesomeIcon   className = "pe-1" icon={faPhoneSquareAlt} size="2x"/>
                 <h5>0175558211, info@flyfarladies.com</h5> 
             </Col>
 
-            <Col   className="d-flex justify-content-end">
+            <Col   lg="6" md="6" sm="12" xs="12"  className="d-flex justify-content-end">
 
                 <Button  className ="rounded-pill border-white" variant="dark">Login/Register</Button>
                 
@@ -26,6 +40,7 @@ const Topnavbar = () => {
                 
                 
         </Row>
+        */}
             
     </Container>
 
