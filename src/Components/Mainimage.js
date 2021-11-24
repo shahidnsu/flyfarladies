0bd3c9fb24169c0a958fb2a214f0a8add2b2d263
Shahidnsu <shahid.khan@northsouth.edu>
2021-11-25 02:13:17 +0600

    completed the stock photos options

--- src/Components/Mainimage.js
+++ src/Components/Mainimage.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Container,Row,Col } from "react-bootstrap";
-import Slideshow  from "./Slideshow";
+import { Container,Row,Col, Carousel } from "react-bootstrap";
+import '../assets/style/Mainimage.css'
 import test from '../assets/images/test.jpeg';
 import Mainimageslide from './Mainimageslide'
 
@@ -9,18 +9,88 @@ const MainImage =() => {
     return (
         <div>
             <Container fluid>
-                <Row className="mt-5">
-                    <Col>
-
-                    <p className="h2 text-center">
-                    Stock Images
+            <Row className="mt-5 ">
+                <Col>
+                    <p className="main-image-heading text-center">Stock Images</p>
+                    <p className="text-center">The best source of tour stock image in Bangladesh. We collect pictures by professionals. 
+                    Check which images are perfect <br></br>for you.
                     </p>
-                    <p className="h5 text-center">
-                        The best source of tour stock image in Bangladesh. We collect pictures by professionals. Check which images are perfect for you.</p>
-                    </Col>
-                </Row>
+                </Col>
+            </Row>
+            <Row className="mx-2">
+            <Col className="d-flex">
+                <Carousel variant 
+                indicators={false}
+                controls={false}> 
+                    <Carousel.Item>
+                    <div className="d-flex">
+                    <div className="col-md-3 col-lg-3 col-sm-12 col-xs-12 shadow">
+                    
+                    <img  className ="w-75" src={test} alt="testing purpose"/>
+
+                    </div>
+
+
+                      <div className="col-md-3 col-lg-3 col-sm-12 col-xs-12 shadow">                   
+
+                    <img  className ="w-75 ms-1" src={test} alt="testing purpose"/>
+                    </div>
+
+                    <div className="col-md-3 col-lg-3 col-sm-12 col-xs-12 shadow">
+
+                    <img  className="w-75 ms-1" src={test} alt="testing purpose"/>
+                    </div>
+
+                    <div className="col-md-3 col-lg-3 col-sm-12 col-xs-12 shadow">
+
+                    <img  className="w-75 ms-1" src={test} alt="testing purpose"/>
+                    </div>
+                    </div>
+
+                    </Carousel.Item>
+
+
+                    <Carousel.Item>
+                    <div className="d-flex">
+                    <div className="col-md-3 col-lg-3 col-sm-12 col-xs-12 shadow">
+                    
+                    <img  className ="w-75" src={test} alt="testing purpose"/>
+
+                    </div>
+
+
+                      <div className="col-md-3 col-lg-3 col-sm-12 col-xs-12 shadow">                   
+
+                    <img  className ="w-75 ms-1" src={test} alt="testing purpose"/>
+                    </div>
+
+                    <div className="col-md-3 col-lg-3 col-sm-12 col-xs-12 shadow">
+
+                    <img  className="w-75 ms-1" src={test} alt="testing purpose"/>
+                    </div>
+
+                    <div className="col-md-3 col-lg-3 col-sm-12 col-xs-12 shadow">
+
+                    <img  className="w-75 ms-1" src={test} alt="testing purpose"/>
+                    </div>
+                    </div>
+
+                    </Carousel.Item>
+
+                </Carousel>
+
+                </Col>
+            </Row>
+            <Row className="mt-4 border-bottom">
+                <Col>
+                    <p className="text-center">
+                        Browse all Stock photos
+                    </p>
+                </Col>
+            </Row>
+                
             </Container>
-           <Slideshow item ={<Mainimageslide testImg = {test} />}  />
+           
         </div>
     )
 
