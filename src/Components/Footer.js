2fa8feed5adac2256473d1f010c816b502fcec54
Shahidnsu <shahid.khan@northsouth.edu>
2021-11-26 02:13:06 +0600

    solved all the issues of the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,14 +9,14 @@ const Footer =() => {
 
     return(
         <div>
-           <Container fluid className="mt-5">
+           <Container fluid className="mt-5 shadow-lg">
           
 
            <Row>
            
 
            <Col md="3" lg="3" sm="12" > 
-           <p className="h5 mb-4"> YOUR NEXT ADVENTURE</p>
+           <p className="h5 mt-5  text-center"> YOUR NEXT ADVENTURE</p>
            <Stack gap={3}  className="justify-content-sm-center d-flex">
                     <ul className="list-group list-group-flush">
                     <input type="text" className="list-group-item" placeholder="First Name.."></input>
@@ -32,7 +32,7 @@ const Footer =() => {
 
            </Col>
            <Col md="3" lg="3" sm="12" className="mt-5" >
-           <Stack gap={3} className=" mx-3 ">
+           <Stack gap={3} className=" mt-4 mx-3 ">
                     <ul className="list-group list-group-flush">
                          <input type="text" className="list-group-item" placeholder="Last Name.."></input>
                          <input type="tel" className="list-group-item" placeholder="Contact No.."></input>
@@ -47,8 +47,8 @@ const Footer =() => {
            </Col>
            <Col md="3" lg="3" sm="12">
 
-           <p className="h5 text-center">QUICK LINKS</p>
-                <div className="mt-3 mx-3">
+           <p className="h5 text-center mt-5">QUICK LINKS</p>
+                <div className="mt-2 mx-3">
                    <Stack gap={3}>
                    <ul className="list-group list-group-flush">
                         <li className="list-group-item">Home</li>
@@ -62,7 +62,7 @@ const Footer =() => {
                
            </Col>
            <Col md="3" lg="3" sm="12">
-           <p className="h5">SOCIAL WITH US</p>
+           <p className="h5 mt-5 ">SOCIAL WITH US</p>
 
                 <Stack direction ="horizontal" gap={3} className="mt-4">
                   <FontAwesomeIcon icon={faFacebook} size="2x" />
@@ -78,55 +78,7 @@ const Footer =() => {
            
                 
                 
-                {/*<Col  md="4" lg="4" sm="12" >
-                    <p className="h2 text-center"> READY FOR NEXT ADVENTURE?</p>
-                    <div className="d-flex mt-5">
-                    <Stack gap={3} className="col-md-2 col-sm-12 col-xs-12 col-lg-2">
-                    <ul className="list-group list-group-flush">
-                    <input type="text" className="list-group-item" placeholder="First Name.."></input>
-                    <input type="email" className="list-group-item" placeholder="Email ID..."></input>
-                    <input type="date" className="list-group-item" placeholder="Travel Date.."></input>
-                    <input type="textarea" className="list-group-item" placeholder="Tell us about your dream adventure"></input>
-                    
-                          
-    </ul>  
-                        
-                    </Stack>
-
-                    
-                    </div>
-                </Col>
-
-                <Col md={2} lg={2} sm={12} xs={12} className="mt-1">
-
-                <p className="h2 text-center">QUICK LINKS</p>
-                <div className="mt-3 mx-3">
-                   <Stack gap={3}>
-                   <ul className="list-group list-group-flush">
-                        <li className="list-group-item">Home</li>
-                      <li className="list-group-item">About Us</li>
-                    <li className="list-group-item">Contact Us</li>
-                     <li className="list-group-item">Terms and Conditions</li>
-                     <li className="list-group-item">Refund Policy </li>
-</ul>
-                   </Stack>
-                </div>
-
-                </Col>
-                <Col md={3} xs={12} sm={12} lg={3} className="mt-1">
-                <p className="h2">SOCIAL WITH US</p>
-
-                <Stack direction ="horizontal" gap={3} className="mt-4">
-                  <FontAwesomeIcon icon={faFacebook} size="2x" />
-                 <FontAwesomeIcon icon ={faYoutube} size="2x"/>
-                 <FontAwesomeIcon icon= {faLinkedin} size="2x"/>
-                 <FontAwesomeIcon icon ={faTwitter} size="2x"/>
-                 <FontAwesomeIcon icon ={faGoogle} size="2x"/>
-
-                  </Stack>
-
-                </Col>
-                */}
+                
 
 
                 </Row>
