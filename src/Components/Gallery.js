6cf2486c630636f1e9b08032efa6774842b09bae
Shahidnsu <shahid.khan@northsouth.edu>
2021-11-30 22:02:20 +0600

    completed  the gallery page need to work on the desgin side

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,26 +1,38 @@
 import React from "react";
 import { Container,Row,Col } from "react-bootstrap";
 import test from '../assets/images/test.jpeg';
+import '../assets/style/Gallery.css';
 
 const Gallery = () => {
     const heading = 'Photo gallery'
     return(
         <>
-        <h1 className="text-center">{heading}</h1>
-        <Container fluid>
-            <Row>
-            <Col className="d-flex" lg="4" md="4" sm="12">
+       
+        <Container >
+        <h1 className="mb-5 mt-4 text-center me-4">{heading}</h1>
+            <Row >
+            <Col  lg="3" md="3" sm="12" className="shadow">
             
                 
                     
-                        <img  className ="w-50 h-100 me-2" src={test} alt="test" />
-                        <p>Bangladesh khulna</p>
-                        <div className="d-flex flex-column ">
-                        <img  className ="w-50 h-75 mb-2" src={test} alt="test" />
-                        <img  className ="w-50 h-75 mb-2" src={test} alt="test" />
-                        <img  className ="w-50  h-75 mb-2" src={test} alt="test" />
+                       <div className ="d-flex ">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
                         
-                        </div>
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
 
                         
                         
@@ -28,11 +40,205 @@ const Gallery = () => {
                     
                 
             </Col>
-            <Col>
-                2
+            <Col lg="3" md="3" sm="12" className="shadow ms-2">
+                
+
+                 <div className ="d-flex">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
+                        
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
             </Col>
-            <Col>
-                  3
+            <Col lg="3" md="3" sm="12" className="shadow ms-2">
+            <div className ="d-flex">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
+                        
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
+            </Col>
+            
+
+            </Row>
+
+
+
+            
+            <Row  className="mt-4">
+            <Col  lg="3" md="3" sm="12" className="shadow">
+            
+                
+                    
+                       <div className ="d-flex ">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1 ">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
+                        
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
+
+                        
+                        
+
+                    
+                
+            </Col>
+            <Col lg="3" md="3" sm="12" className="shadow ms-2">
+                
+
+                 <div className ="d-flex">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
+                        
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
+            </Col>
+            <Col lg="3" md="3" sm="12" className="shadow ms-2">
+            <div className ="d-flex">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
+                        
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
+            </Col>
+            
+
+            </Row>
+
+
+
+            
+            <Row  className="mt-4">
+            <Col  lg="3" md="3" sm="12" className="shadow">
+            
+                
+                    
+                       <div className ="d-flex ">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
+                        
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
+
+                        
+                        
+
+                    
+                
+            </Col>
+            <Col lg="3" md="3" sm="12" className="shadow ms-2">
+                
+
+                 <div className ="d-flex">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
+                        
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
+            </Col>
+            <Col lg="3" md="3" sm="12" className="shadow ms-2">
+            <div className ="d-flex">
+                           <img src={test} className="w-75" alt="Nepal tour" />
+                          
+
+                           <div className="d-flex flex-column ms-1">
+                              
+                           <img src={test} className="w-100" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           <img src={test} className="w-100 mt-1" alt="Nepal tour" />
+                           </div>
+
+                        
+                       </div>
+
+                       <div className="d-flex flex-column">
+                           <span className="gallery-label text-center mt-4">BALI, INDONESIA</span>
+                           <span className="gallery-label-button text-center w-2 mx-auto mb-4">View</span>
+                       </div>
             </Col>
             
 
