5ee0a1af3094758c124dd5bfaa293e293a6a47e4
Shahidnsu <shahid.khan@northsouth.edu>
2021-12-06 21:52:01 +0600

    completedt the job page

--- src/Components/Job.js
+++ src/Components/Job.js
@@ -4,6 +4,8 @@ import '../assets/style/Job.css'
 import bag from '../assets/images/bag.png'
 import location from '../assets/images/location.png'
 import product from '../assets/images/product.svg'
+import uploadCv from '../assets/images/uploadcv1.svg'
+import uploadCv1 from '../assets/images/uploadcv2.svg'
 
 
 const Job = () => {
@@ -92,6 +94,51 @@ const Job = () => {
             <Col className= "border-end p-3">
                 
             </Col>
+
+        </Row>
+
+        {/*
+        
+
+        upload section 
+        
+        
+        
+        */}
+
+        <Row className="mt-2 mx-2 d-flex justify-content-between" >
+        <Col className="border rounded-1 p-4" sm='12' md='5' lg='5'>
+        <Row>
+            <Col className="d-flex flex-column ">
+                <span className="upload-cv-heading">Upload your CV</span>
+                <span className="upload-cv-text">
+                    Find your dream job with  Flyfar Ladies
+                </span>
+
+                
+            </Col>
+            <Col className="d-flex justify-content-end">
+              <img src={uploadCv} alt="upload cv" />
+            </Col>
+        </Row>
+            
+        </Col>
+        <Col className="border rounded-1 p-4" sm='12' md='5' lg='5'>
+        <Row>
+            <Col className="d-flex flex-column">
+                <span className="upload-cv-heading">Post  a job</span>
+                <span className="upload-cv-text">
+                   Find the perfect Candidate.
+                </span>
+
+                
+            </Col>
+            <Col className="d-flex justify-content-end">
+              <img src={uploadCv1} alt="upload cv" />
+            </Col>
+        </Row>
+            
+        </Col>
 
         </Row>
 
