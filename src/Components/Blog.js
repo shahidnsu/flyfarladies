8b90f4de50aae9059b7fb7b7df2e6c608c676152
Shahidnsu <shahid.khan@northsouth.edu>
2021-12-07 00:54:15 +0600

    done with blog section

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -5,53 +5,53 @@ import blogImage from '../assets/images/BlogImage.jpeg'
 
 export default function Blog() {
   return (
-    <div>
-      <Container>
+    <>
+      <Container fluid>
         <Row>
           <Col>
-            <p className="text-center blog-section-heading">Our Blogs</p>
+            <h1 className="text-center blog-section-heading">Our Blogs</h1>
           </Col>
         </Row>
 
         <Row className="mt-5">
-          <Col >
+          <Col>
             <Card style={{ width: "22rem", height:"23rem"}} className="shadow" >
               <Card.Img variant="top" src={blogImage} />
               <Card.Body>
                 
                 <Card.Text >
-                <p className="blog-section-text">10 BEST DAY TRIPS FROM DHAKA</p>
-                  <p className="blog-section-date">November 10, 2021</p>
+                <h2 className="blog-section-text">10 BEST DAY TRIPS FROM DHAKA</h2>
+                  <span className="blog-section-date">November 10, 2021</span>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col>
+          <Col  >
             <Card style={{ width: "22rem", height: "23rem" }} className="shadow">
               <Card.Img variant="top" src={blogImage} />
               <Card.Body>
                
                 <Card.Text>
-                <p className="blog-section-text"> BEST TURKEY  TOUR PACKAGES FROM BANGLADESH</p>
-                  <p className="blog-section-date">October 25, 2021</p>
+                <h2 className="blog-section-text"> BEST TURKEY  TOUR PACKAGES FROM BANGLADESH</h2>
+                  <span className="blog-section-date">October 25, 2021</span>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
-          <Col>
+          <Col  >
             <Card style={{ width: "22rem", height: "23rem" }} className="shadow">
               <Card.Img variant="top" src={blogImage} />
               <Card.Body>
                 
                 <Card.Text>
-                  <p className="blog-section-text">THREE YEARS OF FLYING HIGH WITH FLY FAR LADIES</p>
-                  <p className="blog-section-date">October 03, 2021</p>
+                  <h2 className="blog-section-text">THREE YEARS OF FLYING HIGH WITH FLY FAR LADIES</h2>
+                  <span className="blog-section-date">October 03, 2021</span>
                 </Card.Text>
               </Card.Body>
             </Card>
           </Col>
         </Row>
       </Container>
-    </div>
+    </>
   );
 }
