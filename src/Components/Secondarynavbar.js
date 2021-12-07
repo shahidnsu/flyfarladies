3868824f072387f5347129fac9d8891a8a564375
Shahidnsu <shahid.khan@northsouth.edu>
2021-12-08 00:26:41 +0600

    added some css for secondarynavbar

--- src/Components/Secondarynavbar.js
+++ src/Components/Secondarynavbar.js
@@ -11,28 +11,28 @@ const Secondarynavbar = () =>
      <div>
 
    
-   <Navbar  expand="lg" className="sticky-top">
+   <Navbar  expand="lg" >
   <Container fluid className="mt-3 ps-5">
     <Navbar.Brand  className ="text-dark h5" href="#home"><img src={logo} alt="logo" /> </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto ">
-      <Nav.Link    as ={Link} to="/" className="text-dark h5 nav-link-a" >Home</Nav.Link>
+      <Nav   className="me-auto ">
+      <Nav.Link as ={Link} to="/" className="text-dark h5 nav-link-a hover-effect" >Home</Nav.Link>
 
-        <Nav.Link  as ={Link} to="/gallery" className="text-dark h5 nav-link-a">Gallery</Nav.Link> 
-        <Nav.Link  className="text-dark h5 nav-link-a" href="#stockPhotos">Stock Photos</Nav.Link>
-        <Nav.Link  as ={Link} to ="/job" className="text-dark h5 nav-link-a" href="#job">Job</Nav.Link>
-        <Nav.Link  as ={Link} to="/blog" className="text-dark h5 nav-link-a" href="#blog">Blog</Nav.Link>
-        <Nav.Link  className="text-dark h5 nav-link-a" href="#packages">Packages</Nav.Link>
-        <Nav.Link className="text-dark  h5 nav-link-a" href="#travel">Travel</Nav.Link>
-        <NavDropdown title="Travel Shopping" id="basic-nav-dropdown" className="text-dark h5 nav-link-a">
-          <NavDropdown.Item className="text-dark h5" href="#action/3.1">Swim suits</NavDropdown.Item>
-          <NavDropdown.Item  className="text-dark h5" href="#action/3.2">Mask</NavDropdown.Item>
-          <NavDropdown.Item className="text-dark h5" href="#action/3.3">Poster</NavDropdown.Item>
+        <Nav.Link  as ={Link} to="/gallery" className="text-dark h5 nav-link-a hover-effect">Gallery</Nav.Link> 
+        <Nav.Link  className="text-dark h5 nav-link-a hover-effect" href="#stockPhotos">Stock Photos</Nav.Link>
+        <Nav.Link  as ={Link} to ="/job" className="text-dark h5 nav-link-a hover-effect" href="#job">Job</Nav.Link>
+        <Nav.Link  as ={Link} to="/blog" className="text-dark h5 nav-link-a hover-effect" href="#blog">Blog</Nav.Link>
+        <Nav.Link  className="text-dark h5 nav-link-a hover-effect" href="#packages">Packages</Nav.Link>
+        <Nav.Link className="text-dark  h5 nav-link-a hover-effect" href="#travel">Travel</Nav.Link>
+        <NavDropdown title="Travel Shopping " id="basic-nav-dropdown" className="text-dark h5 nav-link-a hover-effect">
+          <NavDropdown.Item className="text-dark h5 hover-effect" href="#action/3.1">Swim suits</NavDropdown.Item>
+          <NavDropdown.Item  className="text-dark h5 hover-effect" href="#action/3.2">Mask</NavDropdown.Item>
+          <NavDropdown.Item className="text-dark h5 hover-effect" href="#action/3.3">Poster</NavDropdown.Item>
           
-          <NavDropdown.Item className="text-dark h5" href="#action/3.4">Sight Bad</NavDropdown.Item>
-          <NavDropdown.Item className="text-dark h5" href="#action/3.4">Passport Holder</NavDropdown.Item>
-          <NavDropdown.Item className="text-dark h5" href="#action/3.4">DFBDZ</NavDropdown.Item>
+          <NavDropdown.Item className="text-dark h5 hover-effect" href="#action/3.4">Sight Bad</NavDropdown.Item>
+          <NavDropdown.Item className="text-dark h5 hover-effect" href="#action/3.4">Passport Holder</NavDropdown.Item>
+          <NavDropdown.Item className="text-dark h5 hover-effect" href="#action/3.4">DFBDZ</NavDropdown.Item>
         </NavDropdown>
 
         
