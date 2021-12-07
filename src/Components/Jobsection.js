c9854caf711f4a538c359073107bbc79eb3b3220
Shahidnsu <shahid.khan@northsouth.edu>
2021-12-08 01:58:11 +0600

    added images and styles for job section

--- src/Components/Jobsection.js
+++ src/Components/Jobsection.js
@@ -1,32 +1,42 @@
-import React from 'react';
-import { Container,Row,Col } from 'react-bootstrap';
-import Jobsectioncard from './Jobsectioncard';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Jobsectioncard from "./Jobsectioncard";
 
-import Slideshow from './Slideshow';
+import Slideshow from "./Slideshow";
 
-const Jobsection = () => 
-{
-    return (
-        <>
+const Jobsection = () => {
+  return (
+    <>
+      <Container fluid>
+        <Row className="mt-5  ">
+          <Col>
+            <p
+              className=" text-center"
+              style={{
+                fontSize: "16px",
+                textAlign: "center",
+                fontWeight: "500",
+              }}
+            >
+              Employee Offering job
+            </p>
+            <p
+              className=" text-center"
+              style={{
+                fontSize: "32px",
+                textAlign: "center",
+                fontWeight: "700",
+              }}
+            >
+              Here's Features Job
+            </p>
+          </Col>
+        </Row>
+      </Container>
 
-            <Container fluid>
-            <Row className="mt-5  ">
-            <Col>
+      <Slideshow item={<Jobsectioncard name="Accounting" number="5" />} />
+    </>
+  );
+};
 
-                <p className="h4 text-center">Employee Offering job</p>
-                <p className="h1 text-center">Here's Features Job</p>
-                </Col>
-                </Row>
-            </Container>
-            
-
-            <Slideshow item = {<Jobsectioncard name="Accounting" number="5"  />} />
-            
-            
-
-            
-        </>
-    )
-}
-
-export default Jobsection
+export default Jobsection;
--- src/Components/Jobsectioncard.js
+++ src/Components/Jobsectioncard.js
@@ -1,9 +1,8 @@
 
 import React from "react";
 import { Card } from "react-bootstrap";
-import test from '../assets/images/test.jpeg';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBookmark } from "@fortawesome/free-regular-svg-icons";
+import featureJob from '../assets/images/featureJob.jpeg';
+import checklist from '../assets/images/checklist.svg';
 import '../assets/style/jobsectioncard.css';
 
 const Jobsectioncard =({name,number}) => {
@@ -11,7 +10,9 @@ const Jobsectioncard =({name,number}) => {
 
         <div>
              <Card  className ="shadow"style={{ width: '20rem' ,height : '27rem'}}>
-       <Card.Img variant="top" src={test} />
+       <Card.Img variant="top" src={featureJob} style={{
+         height: '100%'
+       }} />
          <Card.Body className="job-section-details">
          
           
@@ -28,7 +29,9 @@ const Jobsectioncard =({name,number}) => {
 
         <div >
 
-        <p className="mt-2 h5 job-section-details-text">  <FontAwesomeIcon icon={faBookmark} size="2x"/></p>
+        <p className="mt-2 h5 job-section-details-text">  <img src={checklist} alt="checklist" style= {{
+          width: '40px'
+        }} /></p>
         
 
         </div>
