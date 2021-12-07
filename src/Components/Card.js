2c302f49168995d2194da34e126acc883a44e2a2
Shahidnsu <shahid.khan@northsouth.edu>
2021-12-08 01:30:09 +0600

    redesgin the toursection.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,51 +1,101 @@
 import React from "react";
-import { faCircleNotch,  faUsers } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import test from "../assets/images/test.jpeg"
-import {Card}  from "react-bootstrap";
-import '../assets/style/card.css';
-const card = ({des, price, day,people}) => {
+import user from "../assets/images/user.svg";
+import tourImage from "../assets/images/tour-image.jpeg";
+import refresh from "../assets/images/refrash.svg";
+import { Card } from "react-bootstrap";
+import "../assets/style/card.css";
+const card = ({ des, price, day, people }) => {
+  return (
+    <>
+      <Card className="shadow " style={{ width: "20rem", height: "30rem" }}>
+        <Card.Img
+          variant="top"
+          src={tourImage}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+        />
+        <Card.Body>
+          <div className="d-flex justify-content-between">
+            <Card.Title
+              style={{
+                fontSize: "20px",
+                fontWeight: "600",
+                paddingBottom: "10px",
+              }}
+            >
+              {des}
+            </Card.Title>
+            <p
+              className="card-text"
+              style={{
+                fontSize: "22px",
+                fontWeight: "600",
+                color: "#632065",
+              }}
+            >
+              {price}
+            </p>
+          </div>
 
-    return(
-        <>
+          <Card.Text
+            style={{
+              color: "#4A4A4A",
+              lineHeight: "25px",
+            }}
+          >
+            Explore Turkey in the most beautiful way
+          </Card.Text>
+          <div className="d-flex justify-content-between">
+            <div className="d-flex justify-content-start">
+              <p
+                className="mt-2 h5"
+                style={{
+                  display: "inline-block",
+                  fontSize: "15px",
+                  marginRight: "25px",
+                }}
+              >
+                <img
+                  src={refresh}
+                  alt="refresh"
+                  style={{
+                    width: "17px",
+                    height: "17px",
+                    marginRight: "10px",
+                  }}
+                />{" "}
+                {day} days tours{" "}
+              </p>
+            </div>
 
-      
-                 <Card className="shadow " style={{ width: '20rem' ,height : '30rem'}}>
-       <Card.Img variant="top" src={test} />
-         <Card.Body>
-         <div className="d-flex justify-content-between">
-
-         <Card.Title>{des}</Card.Title>
-         <p className="card-text">{price}</p>
-         </div>
-          
-           <Card.Text>
-             Explore Turkey in the most beautiful way
-       </Card.Text>
-    <div className="d-flex justify-content-between">
-        <div className="d-flex justify-content-start">
-
-        <p className="mt-2 h5"><FontAwesomeIcon icon ={faCircleNotch} size="1x" /> {day} days tour </p>
-        
-
-        </div>
-
-        <div className="d-flex justify-content-start">
-
-        <p className="mt-2 h5"> <FontAwesomeIcon icon ={faUsers} size="1x" />{people} persons </p>
-        
-
-        </div>
-
-
-    </div>
-  </Card.Body>
-</Card>
-                 
-             
-
-
-        </>
-    )
-}
+            <div className="d-flex justify-content-start">
+              <p
+                className="mt-2 h5"
+                style={{
+                  display: "inline-block",
+                  fontSize: "15px",
+                  marginRight: "25px",
+                }}
+              >
+                {" "}
+                <img
+                  src={user}
+                  alt="user"
+                  style={{
+                    width: "17px",
+                    height: "17px",
+                    marginRight: "10px",
+                  }}
+                />
+                {people} Max{" "}
+              </p>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 export default card;
--- src/Components/Toursection.js
+++ src/Components/Toursection.js
@@ -1,44 +1,57 @@
-
 import React from "react";
-import { Container,Row,Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import Card from "./Card";
 
-
 const Toursection = () => {
-
-    return (
-       
-
-        <div>
-            <Container fluid>
-                <Row className="mt-5">
-                    <Col>
-                        <p className="h2 text-center">
-                        Most Popular Tours
-                        </p>
-                        <p className="text-center mt-3">
-                        Check out the Most Popular Tours deals below to book your holiday for the best price! Various promotions run throughout the year, so keep an eye out to bag the hottest of bargains for unforgettable experiences.
-                        </p>
-                    </Col>
-                </Row>
-            </Container>
-            <Container fluid >
-                <Row className="d-flex flex-row ms-1">
-                    <Col md={4} lg={4} xs={12} sm={12} className=" justify-content-sm-center">
-                        <Card des="Ankara" price="$20000" day="4" people="5" />
-                    </Col>
-                    <Col md={4} lg={4} xs={12} sm={12}  >
-                    <Card des="Khulna" price="$2000" day="3" people="7" />
-                    </Col>
-                    <Col md={4} lg={4} xs={12} sm={12}>
-                    <Card des="Khulna" price="$2000" day="3" people="7" />
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
-
-
-}
-export default Toursection
+  return (
+    <div>
+      <Container fluid>
+        <Row className="mt-5">
+          <Col>
+            <p
+              className=" text-center"
+              style={{
+                fontSize: "35px",
+                fontWeight: "600",
+                paddingBottom: "15px",
+              }}
+            >
+              Most Popular Tours
+            </p>
+            <p
+              className="text-center mt-3"
+              style={{
+                maxWidth: "70%",
+                margin: "0 auto",
+                padding: "0",
+                border: "0",
+                fontSize:"14px",
+                fontWeight:"500"
+              }}
+            >
+              Check out the Most Popular Tours deals below to book your holiday
+              for the best price! Various promotions run throughout the year, so
+              keep an eye out to bag the hottest of bargains for unforgettable
+              experiences.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <Container fluid>
+        <Row className="d-flex flex-row ms-1 mt-4">
+          <Col
+            md={4}
+            lg={4}
+            xs={12}
+            sm={12}
+            className=" justify-content-sm-center"
+          >
+            <Card des="Ankara" price="$20000.00" day="4" people="20" />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+export default Toursection;
